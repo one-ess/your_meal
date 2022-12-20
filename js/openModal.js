c5b1eb297cf7bc667b5e5fc3a841dd1e0d8c91b6
TArtur <onese7enzzz@gmail.com>
2022-12-21 02:32:22 +0600

    first commit

---
+++ js/openModal.js
@@ -0,0 +1,35 @@
+import { API_URL, PREFIX_PRODUCT } from "./const.js";
+import { getData } from "./getData.js";
+import {
+  modalProduct,
+  ingredientsList,
+  modalProductTitle,
+  modalProductPriceCount,
+  modalProductImg,
+  modalProductDescription,
+  ingredientsCalories,
+  modalProductBtn,
+  countAmount,
+} from "./DOMvars.js";
+
+export const openModal = async (id) => {
+  const product = await getData(`${API_URL}${PREFIX_PRODUCT}/${id}`);
+
+  const ingredientListItems = product.ingredients.map((ingredient) => {
+    const li = document.createElement("li");
+    li.classList.add("ingredients__item");
+    li.textContent = ingredient;
+    return li;
+  });
+
+  modalProductTitle.textContent = product.title;
+  modalProductImg.src = `${API_URL}/${product.image}`;
+  modalProductDescription.textContent = product.description;
+  modalProductPriceCount.textContent = product.price;
+  modalProductBtn.dataset.idProduct = product.id;
+  ingredientsList.textContent = ``;
+  ingredientsList.append(...ingredientListItems);
+  ingredientsCalories.textContent = `${product.weight}г, ккал ${product.calories}`;
+  modalProduct.classList.add("modal_open");
+  countAmount.textContent = 1;
+};
